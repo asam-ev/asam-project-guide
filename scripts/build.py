56fa0d88d4bb3488dacbe50abd57360584790bf5
Philip Windecker <philip.windecker@avenyr.de>
2021-11-05 14:08:13 +0100

    Added try-except-handling for build.py

--- scripts/build.py
+++ scripts/build.py
@@ -12,23 +12,42 @@ def main():
 
 def reference_macro_replacement():
     print("CREATE REFERENCES BY ATTRIBUTES")
-    os.system("python create_references_by_attributes.py -p doc/modules")
+    try:
+        os.system("python create_references_by_attributes.py -p doc/modules")
+    except:
+        pass
 
 def nav_adoc_creation():
     print("CREATE NAV.ADOC FOR FOLDER")
-    os.system("python create_nav_adoc_for_folder.py -p doc/modules/ROOT/pages/")
+    try:
+        os.system("python create_nav_adoc_for_folder.py -p doc/modules/ROOT/pages/")
+    except:
+        pass
 
 def run_docker_compose():
     print("RUN DOCKER-COMPOSE")
-    os.chdir("../")
-    # print(os.getcwd())
-    os.system("docker-compose run custom-lunr")
+    try:
+        os.system("docker-compose run custom-lunr")
+    except:
+        try:
+            os.chdir("../")
+            # print(os.getcwd())
+            os.system("docker-compose run custom-lunr")
+        except:
+            pass
+
 
 def cleanup():
     print("CLEANUP AFTER BUILD")
-    os.chdir("./scripts")
-    # print(os.getcwd())
-    os.system("python cleanup_after_build.py -p doc/modules")
+    try:
+        os.system("python cleanup_after_build.py -p doc/modules")
+    except:
+        try:
+            os.chdir("./scripts")
+            # print(os.getcwd())
+            os.system("python cleanup_after_build.py -p doc/modules")
+        except:
+            pass
 
 def open_project_guide():
     print("OPEN INDEX.HTML")
