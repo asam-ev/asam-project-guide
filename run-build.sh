31cad0a6c8b50fe0f059c1426aa4ed7f32502b42
Stefan Cyliax <stefan.cyliax@asam.net>
2021-12-07 06:56:52 +0100

    enables pipeline build again. Adjustments for Github
    Signed-off-by: Stefan Cyliax <stefan.cyliax@asam.net>

---
+++ run-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script is used tackle a some specifics of the GitHub action used with a docker container. 
+# It copies the cached node-modules from the container to the runner workspace and then triggers the build using npm
+
+cp -r /usr/src/app/. /github/workspace/.
+exec npm run docker-build
