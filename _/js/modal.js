e9134cceabdee1ae996059697c20ef40523659dc
philipwindecker <philipwindecker@users.noreply.github.com>
2022-04-08 19:10:03 +0000

    deploy: 2652bc5de43d6076870a7f7d142a3bb6a2932493

--- _/js/modal.js
+++ _/js/modal.js
@@ -1,21 +1,65 @@
 // Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img_cont = document.getElementById("modal_image");
-var img = img_cont.getElementsByTagName("img")[0]
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+var modalImageZoom = 1.0;
+
+function changeZoom(direction) {
+	if (direction === "+") {
+		modalImageZoom = Math.min(10.0,modalImageZoom + 0.1);
+		setModalImageZoom();
+	}
+	else if (direction === "-") {
+		modalImageZoom = Math.max(modalImageZoom-0.1,0.1);
+		setModalImageZoom();
+	}
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+function setModalImageZoom() {
+	modalImg.height = modalImg.naturalHeight * modalImageZoom;
+	modalImg.width = modalImg.naturalWidth * modalImageZoom;
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+var modal = document.createElement('div');
+modal.className = "modal";
+modal.id = "imageModal";
+var modalImg = document.createElement('img');
+modalImg.id = "modalContentImg";
+modalImg.className = "modal-content";
+modal.appendChild(modalImg);
+var closeButton = document.createElement('span');
+closeButton.className = "close";
+closeButton.onclick = function() {
+	modal.style.display = "none";
+	modalImageZoom = 1.0;
+}
+closeButton.appendChild(document.createTextNode("\u00D7"));
+var zoomInButton = document.createElement('button')
+zoomInButton.id = "modalZoomIn";
+zoomInButton.className = "modalZoomIn"
+zoomInButton.innerHTML = "+"
+zoomInButton.setAttribute("onclick","changeZoom('+');");
+modal.appendChild(zoomInButton)
+var zoomOutButton = document.createElement('button')
+zoomOutButton.id = "modalZoomOut";
+zoomOutButton.className = "modalZoomOut"
+zoomOutButton.innerHTML = "-"
+zoomOutButton.setAttribute("onclick","changeZoom('-');");
+modal.appendChild(zoomOutButton)
+
+modal.appendChild(closeButton);
+document.body.appendChild(modal);
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgBlocks = document.getElementsByClassName("imageblock")
+for (var block of imgBlocks) {
+    var img =block.getElementsByTagName("img")[0]
+
+    img.onclick = function(){
+        var imageSize = [img.naturalWidth,img.naturalHeight]
+		var zoom = 1.0;
+		modal.style.display = "block";
+        modalImg.src = this.src;
+		modalImg.naturalHeight	= img.naturalHeight;
+		modalImg.naturalWidth	= img.naturalWidth;
+		setModalImageZoom();
+    }
 }
