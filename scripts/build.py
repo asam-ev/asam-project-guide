1351e561062ce1de94cec61d592fb92de73485a5
Philip Windecker <philip.windecker@avenyr.de>
2022-06-01 11:33:13 +0200

    Removed obsolete python scripts (were integrated as extensions already)

--- scripts/build.py
+++ scripts/build.py
@@ -29,34 +29,14 @@ def main(argv):
 
 
     if do_test:
-        # asam_macro_replacement()
-        # nav_adoc_creation()
-        # cleanup()
         exit(0)
 
     if do_start:
-        # asam_macro_replacement()
-        # nav_adoc_creation()
         run_docker_compose()
 
     if do_finish:
-        # cleanup()
         open_project_guide()
         print("BUILD DONE")
-
-def asam_macro_replacement():
-    print("CREATE REFERENCES BY ATTRIBUTES")
-    try:
-        os.system("python asam_antora_macros.py -p doc/modules")
-    except:
-        pass
-
-def nav_adoc_creation():
-    print("CREATE NAV.ADOC FOR FOLDER")
-    try:
-        os.system("python create_nav_adoc_for_folder.py -p doc/modules/compendium/pages/ --module")
-    except:
-        pass
 
 def run_docker_compose():
     print("RUN DOCKER-COMPOSE")
@@ -65,21 +45,7 @@ def run_docker_compose():
     except:
         try:
             os.chdir("../")
-            # print(os.getcwd())
             os.system("docker-compose run antora")
-        except:
-            pass
-
-
-def cleanup():
-    print("CLEANUP AFTER BUILD")
-    try:
-        os.system("python cleanup_after_build.py -p doc/modules")
-    except:
-        try:
-            os.chdir("./scripts")
-            # print(os.getcwd())
-            os.system("python cleanup_after_build.py -p doc/modules")
         except:
             pass
 
@@ -87,7 +53,6 @@ def open_project_guide():
     print("OPEN INDEX.HTML")
     try:
         os.chdir("../site")
-        # print(os.getcwd())
         os.system("start chrome {pos}".format(pos = os.getcwd()+"/index.html"))
     except:
         try:
